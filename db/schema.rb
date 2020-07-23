f8a3cfb59eb938daddac4fc79f9fbfacd589ca4e
hironori sasaki <hrnr.ssk@gmail.com>
2020-07-23 22:31:11 +0900

    nearest_stationsテーブルのマイグレーションファイルを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2020_07_20_183711) do
     t.index ["property_id"], name: "index_nearest_stations_on_property_id"
   end
 
+  create_table "neareststations", force: :cascade do |t|
+    t.integer "property_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_neareststations_on_property_id"
+  end
+
   create_table "properties", force: :cascade do |t|
     t.integer "rent"
     t.text "address"
